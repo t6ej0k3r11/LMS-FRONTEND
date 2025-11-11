c0d3e27f46f4b22bae00b9ce8c3445695631a2fc
T6e_j0k3r <jawaadhossain42@gmail.com>
2025-11-12 05:09:12 +0600

    impliment Toastify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/auth-context/index.jsx";
 import InstructorProvider from "./context/instructor-context/index.jsx";
 import StudentProvider from "./context/student-context/index.jsx";
+import { Toaster } from "@/components/ui/toaster";
 const basename = import.meta.env.DEV ? "" : "/";
 
 createRoot(document.getElementById("root")).render(
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
       <InstructorProvider>
         <StudentProvider>
           <App />
+          <Toaster />
         </StudentProvider>
       </InstructorProvider>
     </AuthProvider>
